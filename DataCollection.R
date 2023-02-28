f574889052f9d1d577ee420aa7b261447d31c21c
FinnW0812 <lc22-0444@lclark.edu>
2023-02-28 20:19:56 +0000

    Created SDM File

--- DataCollection.R
+++ DataCollection.R
@@ -9,6 +9,7 @@ salamanderData<-rawData$gbif$data$Rhyacotriton_cascadae
 noDoopSal <- salamanderData %>% mutate(location = paste(latitude, longitude, dateIdentified, sep = "/")) %>% 
   distinct(location, .keep_all = TRUE)
 noNASal <- noDoopSal %>% filter(latitude != "NA", longitude != "NA")
+
 wrld<-ggplot2::map_data("world")
 xmax<-max(noNASal$longitude)
 xmin<-min(noNASal$longitude)
@@ -16,12 +17,16 @@ ymax<-max(noNASal$latitude)
 ymin<-min(noNASal$latitude)
 ggplot()+
   theme(panel.background = element_rect(fill = "#BFD5E3", colour = "#6D9EC1"))+
-  geom_polygon(data=wrld, mapping=aes(x=long, y=lat, group=group), fill="grey75", colour="grey68") +
+  geom_polygon(data=wrld, mapping=aes(x=long, y=lat, group=group), fill="grey75") +
   geom_point(data=noNASal, mapping=aes(x=longitude, y=latitude),show.legend = FALSE, color="darkgreen")+
-  labs(title="species occurences of Rhyacotriton cascadae", x="longitude", y="latitude")+
+  labs(title="species occurences of Rhyacotriton cascadae from 1818-2023", x="longitude", y="latitude")+
   coord_fixed(xlim=c(xmin -2,xmax +2), ylim=c (ymin,ymax))+
   scale_size_area()+
   borders("state")
+  
+
 
 #the occurrence points are places where a person saw a salamander. 
 #They're verified by several people to be counted as research grade.
+
+
