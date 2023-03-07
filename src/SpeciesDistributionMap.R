bdde0915add4f78bfe0b991f0939b5d503cea2cf
sydneycase1216 <lc22-0362@lclark.edu>
2023-03-07 20:33:07 +0000

    aesthetic fix

--- src/SpeciesDistributionMap.R
+++ src/SpeciesDistributionMap.R
@@ -10,7 +10,7 @@ ggplot()+
   theme(panel.background = element_rect(fill = "#BFD5E3", colour = "#6D9EC1"))+
   geom_polygon(data=wrld, mapping=aes(x=long, y=lat, group=group), fill="grey75") +
   geom_point(data=noNASal, mapping=aes(x=longitude, y=latitude),show.legend = FALSE, color="darkgreen")+
-  labs(title="species occurrences of Rhyacotriton cascadae from 1818-2023", x="longitude", y="latitude")+
+  labs(title="species occurrences of Rhyacotriton \ncascadae from 1818-2023", x="longitude", y="latitude")+
   coord_fixed(xlim=c(xmin -2,xmax +2), ylim=c (ymin,ymax))+
   scale_size_area()+
   borders("state")
