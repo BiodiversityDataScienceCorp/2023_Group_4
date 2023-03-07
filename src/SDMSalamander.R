f8c8007e60ba78567c92688e166bedb66ecaa5fe
FinnW0812 <lc22-0444@lclark.edu>
2023-03-07 19:33:24 +0000

    Future SDM

--- src/SDMSalamander.R
+++ src/SDMSalamander.R
@@ -10,7 +10,7 @@ library(tidyverse)
 library(rJava)
 library(maps)
 
-salamanderDataNotCoords<-noNASal%>%select(longitude,latitude)
+salamanderDataNotCoords<-noNASal%>%dplyr::select(longitude,latitude)
 salamanderDataSpatialPts <- SpatialPoints(salamanderDataNotCoords, proj4string = CRS("+proj=longlat"))
 
 currentEnv <- getData("worldclim", var="bio", res=2.5, path="data/")
@@ -72,3 +72,36 @@ ggplot() +
   theme(legend.box.background=element_rect(),legend.box.margin=margin(5,5,5,5)) 
 
 ggsave(filename="salamanderSDMCurrent.jpg", scale=2, path="output")
+
+futureClim<-raster::getData(name="CMIP5", var = 'bio', res = 2.5,
+                            rcp = 45, model = 'IP', year = 70, path="data")
+names(futureClim) = names(clim)
+
+geographicAreaFuture<-crop(futureClim, predictExtent*1.5)
+
+salamanderFuturePrediction<-raster::predict(salamanderSDM, geographicAreaFuture)
+salamanderSPDF<-as(salamanderFuturePrediction, "SpatialPixelsDataFrame")
+salamanderPredictionDataFrame<-as.data.frame(salamanderSPDF)
+
+
+xmax <- max(salamanderPredictionDataFrame$x)
+xmin <- min(salamanderPredictionDataFrame$x)
+ymax <- max(salamanderPredictionDataFrame$y)
+ymin <- min(salamanderPredictionDataFrame$y)
+
+ggplot() +
+  geom_polygon(data = wrld, mapping = aes(x = long, y = lat, group = group),
+               fill = "grey75") +
+  geom_raster(data = salamanderPredictionDataFrame, aes(x = x, y = y, fill = layer)) + 
+  scale_fill_gradientn(colors = terrain.colors(10, rev = T)) +
+  coord_fixed(xlim = c(xmin, xmax), ylim = c(ymin, ymax), expand = F) +
+  scale_size_area() +
+  borders("world") +
+  borders("state") +
+  labs(title = "SDM of R. cascadae Under CMIP 5 Climate Conditions",
+       x = "longitude",
+       y = "latitude",
+       fill = "Env Suitability") +
+  theme(legend.box.background=element_rect(),legend.box.margin=margin(5,5,5,5)) 
+
+ggsave(filename="salamanderSDMFuture.jpg", path="output", scale=3)
